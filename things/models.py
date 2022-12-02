e84585040f39dc20ea51b53ee3a972d2e66d1fbd
Alexander Grebenyuk <alexander.grebenyuk@kcl.ac.uk>
2022-12-02 18:08:14 +0000

    Exercise 2,3

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Things(AbstractUser):
+    name = models.CharField(unique = True, blank = True, max_length = 30)
+    desciption = models.CharField(max_length=120)
+    quantity = models.IntegerField()
